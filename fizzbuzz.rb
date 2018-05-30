a443c072643bf59e0a2a34325197bb5fd934cb5b
Maria Nunez <mariaalician@gmail.com>
2018-05-30 02:23:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 15 == 0
     "FizzBuzz"
-  else if int % 3 == 0 
+  elsif int % 3 == 0 
     "Fizz"
-  else if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   else nil
   end
